8e10892e806a1ec0859b503a791ef2c0378e0bf4
Steffen Sun Lyng <steffen.lyng@jp.dk>
2020-04-09 22:36:54 +0200

    Added alot more examples of list operations.. including filter and map

--- chp03-next-steps-in-scala/03-creating-and-initializing-a-list/Main.scala
+++ chp03-next-steps-in-scala/03-creating-and-initializing-a-list/Main.scala
@@ -49,3 +49,52 @@ val oneTwoThree = 1 :: 2 :: 3 :: Nil
 //creates a new list, takes constant time.
 val appendedList = 1 :: Nil
 appendedList:+ 3
+
+
+///More list examples
+
+val anEmptyList = List()
+val anotherEmptyList = Nil
+
+val threeItemList = List("A", "B", "C")
+
+val moreItems = "This" :: "Is" :: "A" :: "Long" :: "List" :: Nil
+
+val concatList = threeItemList ::: moreItems
+
+concatList(2) //returns item at second index
+
+concatList.count(item => item.length == 4) //count the items that have a length of 4
+
+concatList.drop(2) //return a list dropping the two first items
+
+concatList.dropRight(2) //return a list dropping the two last items
+
+concatList.exists(item => item == "List") //return a boolean indicating if an item exists with the value "List"
+
+concatList.filter(item => item.length == 4) //return list that contains only items of length 4
+
+concatList.forall(item => item.endsWith("s")) //returns a boolean indicating if all items ends in s
+
+concatList.head //returns the first element in list
+
+concatList.init //returns all BUT the last element
+
+concatList.isEmpty //returns a boolean indicating if the list is empty
+
+concatList.last //returns the last element in the list
+
+concatList.length //returns an Int indicating the length of the list
+
+concatList.map(item => item + "y") //maps each item in the list to a new list of items where "y" is appended
+
+concatList.mkString(" AN DEN ") //returns a string concatenating all items, using the argument as the item-seperator
+println("NO MORE AN DEN!!!!!!")
+
+concatList.filterNot(item => item.length == 4) //reverse filter, returning those that do not match length 4
+
+concatList.reverse //returns the list inverted
+
+concatList.sortWith((s,t) => s.charAt(0).toLower < t.charAt(0).toLower) //sort alphabetically, by first character
+
+concatList.tail //returns the list minus it's first element
