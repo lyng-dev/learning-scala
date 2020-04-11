639649b8dcafaaf4750f36c4ae8c4128de0ea948
Steffen Sun Lyng <steffen.lyng@jp.dk>
2020-04-11 13:37:51 +0200

    Added a few missing files

---
+++ chp04-objects-and-classes/02-a-scala-application/ChecksumAccumulator.java
@@ -0,0 +1,4 @@
+class ChecksumAccumulator() extends scala.AnyRef {
+  def add(b : scala.Byte) : scala.Unit = { /* compiled code */ }
+  def checksum() : scala.Int = { /* compiled code */ }
+}
---
+++ chp04-objects-and-classes/02-a-scala-application/Summer$.java
@@ -0,0 +1,3 @@
+object Summer extends scala.AnyRef {
+  def main(args : scala.Array[_root_.scala.Predef.String]) : scala.Unit = { /* compiled code */ }
+}
