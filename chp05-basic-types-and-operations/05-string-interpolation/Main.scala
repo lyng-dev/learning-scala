1a904354a1bfcdff8b707831a8308aeadf1fe75d
Steffen Sun Lyng <steffen.lyng@jp.dk>
2020-04-11 15:20:45 +0200

    Added some string interpolation and string formatting

---
+++ chp05-basic-types-and-operations/05-string-interpolation/Main.scala
@@ -0,0 +1,21 @@
+
+
+val name = "Steffen"
+
+println(s"Hello $name")
+
+//or
+
+println(s"Another way to say Hello ${name}")
+
+
+//raw output
+
+println(raw"anything here is raw no escapes\\\\\\\\\\")
+
+
+//formatting
+
+println(f"${math.Pi}%.5f")
+
+println(f"${math.Pi}%.97f")
