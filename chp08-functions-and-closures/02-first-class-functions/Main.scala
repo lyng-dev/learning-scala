79132dfa5f94dc2d69c501c172d6f2e6d8372d04
Steffen Sun Lyng <steffen.lyng@jp.dk>
2020-04-13 02:20:58 +0200

    Added examples of function literals, and the simple use of .filter

---
+++ chp08-functions-and-closures/02-first-class-functions/Main.scala
@@ -0,0 +1,22 @@
+val increase = (x: Int) => x + 1
+
+increase(22)
+
+
+//you can make multi-line functions as function literals
+
+val increase = (x: Int) => {
+  println("We")
+  println("are")
+  println("here!")
+  x + 1
+}
+increase(22)
+
+
+//using function literals ie. function values as input
+val someNumbers = List(-11, -10, -5, 0, 5, 10)
+someNumbers.foreach((x: Int) => println(x))
+
+//filter method that takes a function literal, and if it matches, then removes it from the collection
+someNumbers.filter((x: Int) => x > 0)
