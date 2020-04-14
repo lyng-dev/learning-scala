d11e02037a2600d3db85cbd9bfba680627e498b6
Steffen Sun Lyng <steffen.lyng@jp.dk>
2020-04-14 04:51:03 +0200

    Added some details to clear up when things are evaluated

--- chp09-control-abstractions/05-by-name-parameters/Main.scala
+++ chp09-control-abstractions/05-by-name-parameters/Main.scala
@@ -4,3 +4,13 @@ def myAssert(predicate:  => Boolean) =
   if (assertiosnEnabled && !predicate) throw new Exception else true
 
 myAssert(2 < 7)
+
+
+def byNameAssert(predicate: => Boolean) = {
+  println("inside byNameAssert")
+  if (assertiosnEnabled && !predicate)
+    throw new Exception
+  println("After predicate in byNameAssert")
+}
+
+byNameAssert({val x = 2 < 7; println("After comparison in predicate"); x})
